b5e86134f82c1738fc1ec75d9584047037994f89
rahul <rahulsair3000@gmail.com>
2024-01-17 16:56:17 +0530

    Unnesseray sevices removed

--- src/app/mycomponents/services/customer-data.service.ts
+++ src/app/mycomponents/services/customer-data.service.ts
@@ -19,16 +19,7 @@ export class CustomerDataService {
 
   constructor(private http: HttpClient) { }
 
-  getPagedCustomers(page: number, pageSize: number): Observable<any> {
-    const params = new HttpParams()
-      .set('page', page.toString())
-      .set('pageSize', pageSize.toString());
-    
-    return this.http.get<any>(`${this.url}/paged-customers`, { params });
-  }
-  
-  
-
+   
   CustomersWildsearchid(id:any): Observable<Customerdatainterface[]> {
     return this.http.get<Customerdatainterface[]>(`${this.url}/GetAllCustomersData?idSearch=${id}`);
   }
@@ -38,34 +29,18 @@ export class CustomerDataService {
   CustomersWildsearchstate(state:any): Observable<Customerdatainterface[]> {
     return this.http.get<Customerdatainterface[]>(`${this.url}/GetAllCustomersData?stateSearch=${state}`);
   }
-
-
-
   GetAllCustomers(): Observable<Customerdatainterface[]> {
     return this.http.get<Customerdatainterface[]>(`${this.url}/GetAllCustomersData`);
-  }
-
-  GetCustomerById(id: number): Observable<Customerdatainterface[]> {
-    return this.http.get<Customerdatainterface[]>(`${this.url}/GetCustomerWithId/${id}`);
-  }
-
+  }  
   DeleteCustomerById(id: number): Observable<any> {
     return this.http.delete(`${this.url}/DeleteCustomersWithid/${id}`);
   }
-
   AddNewCustomer(newcustomerdata:any): Observable<any> {
-    console.log([newcustomerdata]);
     return this.http.post(`${this.url}/AddNewCustomers`, [newcustomerdata]);
   }
-
-  UpdateCustomer(newcustomerdata:any,id:number): Observable<any> {
-    console.log([newcustomerdata]);
-    console.log(id);    
-  
+  UpdateCustomer(newcustomerdata:any,id:number): Observable<any> { 
     return this.http.put(`${this.url}/UpdateCustomersWithid/${id}`, [newcustomerdata]);
   }
-
-
 
 }
 
