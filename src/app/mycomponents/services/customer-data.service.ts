d93cc1cdf4ba9cbc0deecf3cebc577dbf416d3cd
rahul <rahulsair3000@gmail.com>
2024-01-18 17:33:43 +0530

    change in localhost address

--- src/app/mycomponents/services/customer-data.service.ts
+++ src/app/mycomponents/services/customer-data.service.ts
@@ -13,7 +13,7 @@ import { Customerdatainterface } from '../customerdatainterface';
 })
 export class CustomerDataService {
 
-  private url = "http://192.168.155.26:8008/api/customers";  
+  private url = "http://10.41.148.92:8008/api/customers";  
 
   constructor(private http: HttpClient) { }
    
